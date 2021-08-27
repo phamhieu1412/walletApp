13f52d8e50d38e25b5fd6c5c102518a69b10c35a
TRUNGDONG97 <hieupt@rabiloo.com.vn>
2021-08-27 15:10:25 +0700

    done

--- src/constants/const.js
+++ src/constants/const.js
@@ -1,8 +1,2 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD92uMaApWJm6G_m6YNlIVGqYQgDYJ2Vfk",
-  authDomain: "walletapp-f51d0.firebaseapp.com",
-  projectId: "walletapp-f51d0",
-  storageBucket: "walletapp-f51d0.appspot.com",
-  messagingSenderId: "594377664002",
-  appId: "1:594377664002:web:6f5394439fecdfcd7a691b"
 };
